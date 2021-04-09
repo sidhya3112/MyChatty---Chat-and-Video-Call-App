9e77b43ff2b6fd51bf10e8ba9189bcb601f71829
sidhya <sidhyajain@gmail.com>
2021-04-10 01:21:23 +0530

    Updated Commit

--- lib/screens/pageviews/widgets/online_dot_indicator.dart
+++ lib/screens/pageviews/widgets/online_dot_indicator.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:my_chatty/enum/user_state.dart';
 import 'package:my_chatty/models/user.dart';
 import 'package:my_chatty/resources/firebase_methods.dart';
+import 'package:my_chatty/utils/universal_variables.dart';
 import 'package:my_chatty/utils/utilities.dart';
 
 class OnlineDotIndicator extends StatelessWidget {
@@ -21,9 +22,20 @@ class OnlineDotIndicator extends StatelessWidget {
         case UserState.Offline:
           return Colors.red;
         case UserState.Online:
-          return Colors.green;
+          return UniversalVariables.onlineDotColor;
         default:
           return Colors.orange;
+      }
+    }
+
+    getBorder(int state) {
+      switch (Utils.numToState(state)) {
+        case UserState.Offline:
+          return Border.all(color: Colors.red[700], width: 2);
+        case UserState.Online:
+          return Border.all(color: Colors.green[700], width: 2);
+        default:
+          return Border.all(color: Colors.orange[700], width: 2);
       }
     }
 
@@ -45,6 +57,7 @@ class OnlineDotIndicator extends StatelessWidget {
             width: 10,
             margin: EdgeInsets.only(right: 5, top: 5),
             decoration: BoxDecoration(
+              border: getBorder(user?.state),
               shape: BoxShape.circle,
               color: getColor(user?.state),
             ),
--- lib/screens/pageviews/widgets/user_circle.dart
+++ lib/screens/pageviews/widgets/user_circle.dart
@@ -13,37 +13,22 @@ class UserCircle extends StatelessWidget {
     final UserProvider userProvider = Provider.of<UserProvider>(context);
 
     return Container(
-      height: 44,
-      width: 44,
+      height: 45,
+      width: 45,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
         color: Color.fromRGBO(0, 0, 80, 1),
       ),
-      child: Stack(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              Utils.getInitials(userProvider.getUser.name),
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.blue[100],
-                fontSize: 18,
-              ),
-            ),
+      child: Align(
+        alignment: Alignment.center,
+        child: Text(
+          Utils.getInitials(userProvider.getUser.name),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blue[100],
+            fontSize: 18,
           ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.green[900], width: 2),
-                  color: UniversalVariables.onlineDotColor),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
